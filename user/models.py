98641d45c1fe4c327447436c4a4b889f68f8f6b7
user <user@MacBook-Air--Abdumannon-Rabbimqulov.local>
2026-02-20 15:24:35 +0500

    blog

--- user/models.py
+++ user/models.py
@@ -1,12 +1,22 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
-
+from django.utils import timezone
+from datetime import timedelta
 class CustomUser(AbstractUser):
     phone_number = models.CharField(max_length=15, blank=True, null=True)
     bio = models.TextField(blank=True, null=True)
     avatar = models.ImageField(upload_to='avatars/', blank=True, null=True)
-
+    email = models.EmailField(unique=True)
 
     def __str__(self):
         return self.username
 
+
+class Emailcode(models.Model):
+    users=models.ForeignKey(CustomUser,on_delete=models.CASCADE,related_name='code')
+    code=models.CharField(max_length=6)
+    created_at=models.DateTimeField(auto_now_add=True)
+    def is_valid(self):
+        return timezone.now()<self.created_at+timedelta(minutes=2)
+    def __str__(self):
+        return f" {self.users.username} uchun kod {self.code}"
