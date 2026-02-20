a063b67fac910d32c8271c566686c7e6e6ebeb94
user <user@MacBook-Air--Abdumannon-Rabbimqulov.local>
2026-02-20 16:41:23 +0500

    blog

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,25 @@
+
 from django.db import models
+from user.models import CustomUser
+from post.models import Post
 
 
+class Like(models.Model):
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='post_likes')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ('user', 'post')
+
+class Wishlist(models.Model):
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='user_wishlist')
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='wishlist')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ('user', 'post')
+
+
+    def __str__(self):
+        return f"{self.user.username} saqlagan post: {self.post.title}"
--- conf/urls.py
+++ conf/urls.py
@@ -3,8 +3,12 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('blog/',include('blog.urls')),
+    path('post/',include('post.urls')),
+    path('reply/',include('reply.urls')),
+    path('user/',include('user.urls'))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
